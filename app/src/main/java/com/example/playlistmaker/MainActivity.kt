c3a6e07f6a3dc071ae96706bfdd9c933d354ee21
mfoxtrot <dmitrey.sokolov@gmail.com>
2023-05-10 02:30:44 +0300

    Домашняя работа по спринту 8 "Введение в UI и хранение данных"

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import java.net.Inet4Address
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnSearch = findViewById<Button>(R.id.main_menu_search)
+        btnSearch.setOnClickListener {
+            intent = Intent(this, SearchActivity::class.java)
+            startActivity(intent)
+        }
+
+        val btnMedia = findViewById<Button>(R.id.main_menu_media)
+        btnMedia.setOnClickListener {
+            intent = Intent(this, MediaActivity::class.java)
+            startActivity(intent)
+        }
+
+        val btnSettings = findViewById<Button>(R.id.main_menu_settings)
+        btnSettings.setOnClickListener {
+            intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -0,0 +1,20 @@
+package com.example.playlistmaker
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+        val btnBack = findViewById<View>(R.id.back_button)
+        btnBack.setOnClickListener{
+            intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
