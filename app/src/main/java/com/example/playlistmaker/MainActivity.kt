79da9c316062cc0f94d7daf950fe28e734477b54
mfoxtrot <dmitrey.sokolov@gmail.com>
2023-05-11 00:58:46 +0300

    Реакция на нажатие первой кнопки реализовано через анонимный класс

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -4,28 +4,28 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.Toast
-import java.net.Inet4Address
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnSearch = findViewById<Button>(R.id.main_menu_search)
-        btnSearch.setOnClickListener {
-            intent = Intent(this, SearchActivity::class.java)
-            startActivity(intent)
+        val btnSearch = findViewById<View>(R.id.main_menu_search)
+        val btnSearchClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                intent = Intent(this@MainActivity, SearchActivity::class.java)
+                startActivity(intent)
+            }
         }
+        btnSearch.setOnClickListener(btnSearchClickListener)
 
-        val btnMedia = findViewById<Button>(R.id.main_menu_media)
+        val btnMedia = findViewById<View>(R.id.main_menu_media)
         btnMedia.setOnClickListener {
             intent = Intent(this, MediaActivity::class.java)
             startActivity(intent)
         }
 
-        val btnSettings = findViewById<Button>(R.id.main_menu_settings)
+        val btnSettings = findViewById<View>(R.id.main_menu_settings)
         btnSettings.setOnClickListener {
             intent = Intent(this, SettingsActivity::class.java)
             startActivity(intent)
