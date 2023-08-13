7bf224109950fc49e043857a86267ca6156d3e60
mfoxtrot <dmitrey.sokolov@gmail.com>
2023-08-13 23:51:36 +0300

    Intent actions for settings screen

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,6 +1,7 @@
 package com.example.playlistmaker
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -15,5 +16,45 @@ class SettingsActivity : AppCompatActivity() {
             finish()
         }
 
+        val imgShareApp = findViewById<View>(R.id.shareAppIcon)
+        imgShareApp.setOnClickListener {
+            shareApp()
+        }
+
+        val imgSupport = findViewById<View>(R.id.supportIcon)
+        imgSupport.setOnClickListener {
+            callSupport()
+        }
+
+        val imgUserAgreement = findViewById<View>(R.id.userAgreementIcon)
+        imgUserAgreement.setOnClickListener{
+            openUserAgreement()
+        }
+
+    }
+
+    private fun shareApp(){
+        val intent = Intent(Intent.ACTION_SEND).apply {
+            type = "text/plain"
+            putExtra(Intent.EXTRA_TEXT, getString(R.string.android_course_url))
+        }
+        startActivity(intent)
+    }
+
+    private fun callSupport() {
+        val intent = Intent(Intent.ACTION_SENDTO).apply {
+            data = Uri.parse("mailto:")
+            putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.my_email)))
+            putExtra(Intent.EXTRA_SUBJECT, getString(R.string.support_subject_text))
+            putExtra(Intent.EXTRA_TEXT, getString(R.string.support_body_text))
+        }
+        startActivity(intent)
+    }
+
+    private fun openUserAgreement(){
+        val intent = Intent(Intent.ACTION_VIEW).apply {
+            data = Uri.parse(getString(R.string.user_agreement_url))
+        }
+        startActivity(intent)
     }
 }
