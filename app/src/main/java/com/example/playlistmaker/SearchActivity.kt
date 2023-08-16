90066fd6aa71fb895d3613c411debfb4484af019
mfoxtrot <dmitrey.sokolov@gmail.com>
2023-08-17 00:20:45 +0300

    feat: iTunesSearch API - take 1

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -3,12 +3,19 @@ package com.example.playlistmaker
 import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.MotionEvent
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import androidx.core.widget.doOnTextChanged
 import com.example.playlistmaker.databinding.ActivitySearchBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class SearchActivity : AppCompatActivity() {
 
@@ -17,6 +24,15 @@ class SearchActivity : AppCompatActivity() {
     }
 
     private lateinit var binding: ActivitySearchBinding
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("https://itunes.apple.com")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+    private val itunesSearchService = retrofit.create(ItunesSearchApiService::class.java)
+
+    private val trackList: ArrayList<Track> = arrayListOf()
+    private val adapter = TracksAdapter(trackList)
 
     private var searchString = ""
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,6 +45,8 @@ class SearchActivity : AppCompatActivity() {
         binding.backButton.setOnClickListener() {
             finish()
         }
+
+        binding.searchList.adapter = adapter
 
         binding.searchBox.doOnTextChanged { text, _, _, _ ->
             setEndDrawableVisibility(!text.isNullOrEmpty())
@@ -50,7 +68,15 @@ class SearchActivity : AppCompatActivity() {
             result
         }
 
-        binding.searchList.adapter = TracksAdapter(mockList())
+        binding.searchBox.setOnEditorActionListener { v, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                findTracks(v.text.toString())
+                true
+            }
+            false
+        }
+
+
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
@@ -86,5 +112,34 @@ class SearchActivity : AppCompatActivity() {
             Track("Whole Lotta Love", "Led Zeppelin", "5:33", "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"),
             Track("Sweet Chile O'mine", "Guns N' Roses", "5:03", "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg")
         )
+    }
+
+    private fun findTracks(text: String) {
+        itunesSearchService
+            .searchSongs(text)
+            .enqueue(object :Callback<SearchResponse> {
+                override fun onResponse(
+                    call: Call<SearchResponse>,
+                    response: Response<SearchResponse>
+                ) {
+                    when (response.code()) {
+                        200 -> {
+                            if (response.body()?.results?.isNotEmpty() == true) {
+                                trackList.clear()
+                                trackList.addAll(response.body()?.results!!)
+                                adapter.notifyDataSetChanged()
+                            } else {
+                                Log.e("EMPTY_RESULT", "")
+                            }
+                        }
+                        else -> Log.e("REQUEST_WRONG_STATUS_CODE", response.code().toString())
+                    }
+                }
+
+                override fun onFailure(call: Call<SearchResponse>, t: Throwable) {
+                    Log.e("REQUEST_ERROR", t.message.toString())
+                }
+
+            })
     }
 }
